3edbd210778b5cd2f954f5ef720da8c2f2cb8aa7
Miguel Do carmo tavares da cunha de abreu peixoto <mdo-carm@c1r1s5.42lisboa.com>
2022-11-09 21:18:31 +0000

    still testing code

--- client.c
+++ client.c
@@ -6,7 +6,41 @@
 /*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:23:35 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/10/07 15:23:36 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/10/28 19:10:19 by mdo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	int		bit;
+	int		x;
+	int		i;
+	int	y;
+
+	x = atoi(argv[1]);
+	i = 0;
+	bit = 0;
+	while (argv[2][i])
+	{
+		printf("%c -> ", argv[2][i]);
+		while (bit < 8)
+		{
+			if (argv[2][i] & 128 >> bit) // char(128) binary = 10000000
+				printf("1");
+				// kill(x, 12);//USR2
+			else
+				printf("0");
+				// kill(x, 10);//USR1
+			bit++;
+		}
+		printf("\n");
+		bit = 0;
+		i++;
+	}
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,7 +6,29 @@
 /*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:24:10 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/10/07 15:24:11 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/10/28 17:10:06 by mdo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void usr1handler(int num)
+{
+	write(2, "0", 1);
+}
+
+void usr2handler(int num)
+{
+	write(2, "1", 1);
+}
+
+int	main()
+{
+	printf("PID: %d\n", getpid());
+	signal(SIGUSR1, usr1handler);
+	signal(SIGUSR2, usr2handler);
+	while (1)
+		sleep(1);
+}
