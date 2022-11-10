fe88790821cbda26d6403de6f078cbc64a30d54f
Miguel Do carmo tavares da cunha de abreu peixoto <mdo-carm@c1r1s5.42lisboa.com>
2022-11-10 05:45:39 +0000

    getting somewhere

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:23:35 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/10/28 19:10:19 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/11/10 03:31:18 by mdo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@
 
 int	main(int argc, char **argv)
 {
-	int		bit;
+	int		bit; // position of current bit
 	int		x;
 	int		i;
-	int	y;
 
-	x = atoi(argv[1]);
+	x = atoi(argv[1]); // alterar para ft_atoi
 	i = 0;
 	bit = 0;
 	while (argv[2][i])
@@ -31,12 +30,17 @@ int	main(int argc, char **argv)
 		while (bit < 8)
 		{
 			if (argv[2][i] & 128 >> bit) // char(128) binary = 10000000
+			{
 				printf("1");
-				// kill(x, 12);//USR2
+				kill(x, 12);//SIGUSR2
+			}
 			else
+			{
 				printf("0");
-				// kill(x, 10);//USR1
+				kill(x, 10);//SIGUSR1
+			}
 			bit++;
+			usleep(100);
 		}
 		printf("\n");
 		bit = 0;
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:24:10 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/10/28 17:10:06 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/11/10 05:44:39 by mdo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,60 @@
 #include <unistd.h>
 #include <signal.h>
 
+char	x[8];
+
+void abtoa ()
+{
+	int		binary[8] = {128,64,32,16,8,4,2,1};
+	int		charvalue;
+	int		i;
+	char	y;
+
+	i = 0;
+	while (i <= 0)
+	{
+		if (x[i] == '1')
+		{
+			charvalue = charvalue + binary[i];
+			i++;
+		}
+		if (x[i] == '0')
+			i++;
+	}
+	y = charvalue;
+	// write(2, &x, 1);
+	printf("%d\n", charvalue);
+	// write(2, &y, 1);
+}
+
 void usr1handler(int num)
 {
-	write(2, "0", 1);
+	int		i;
+	
+	i = 0;
+	if (i < 8)
+	{
+		if (num == 10)
+			x[i] = 48; // number 2 in the ascii table
+		if (num == 12)
+			x[i] = 49; // number 1 in the ascii table
+		i++;
+	}
+	x[8] = '\0';
+	abtoa();
 }
 
 void usr2handler(int num)
 {
-	write(2, "1", 1);
+	usr1handler(num);
 }
 
 int	main()
 {
-	printf("PID: %d\n", getpid());
+	printf("PID: %d\n", getpid()); // Change to ft_printf
 	signal(SIGUSR1, usr1handler);
 	signal(SIGUSR2, usr2handler);
+	printf("%s\n", x);
 	while (1)
 		sleep(1);
 }
