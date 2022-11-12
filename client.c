fd62940c95b71527216b0a52bad8c77be5492819
Thorum_Home <miguel_tcap@hotmail.com>
2022-11-12 05:48:43 +0000

    signals arent for constant use

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdo-carm <mdo-carm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thorum <thorum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:23:35 by mdo-carm          #+#    #+#             */
-/*   Updated: 2022/11/11 20:00:36 by mdo-carm         ###   ########.fr       */
+/*   Updated: 2022/11/12 05:43:01 by thorum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,23 @@
 
 int	main(int argc, char **argv)
 {
-	int		bit; // position of current bit
-	int		x;
+	int		bit; //position of current bit
+	int		pid;
 	int		i;
 
-	x = atoi(argv[1]); // alterar para ft_atoi
+	if (argc != 3)
+		return (write(2, "Invalid Arguments\n", 18));
+	pid = atoi(argv[1]); //alterar para ft_atoi
 	i = 0;
 	bit = 0;
-	if (!*argv)
-		return (0);
 	while (argv[2][i])
 	{
 		while (bit < 8)
 		{
-			if (argv[2][i] & 128 >> bit) // char(128) binary = 10000000
-			{
-				kill(x, 12);//SIGUSR2
-			}
+			if (argv[2][i] & 128 >> bit) //char(128) binary = 10000000
+				kill(pid, 12);
 			else
-			{
-				kill(x, 10);//SIGUSR1
-			}
+				kill(pid, 10);
 			bit++;
 			usleep(100);
 		}
